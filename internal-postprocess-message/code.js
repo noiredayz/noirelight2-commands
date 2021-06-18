93c0f79b7fc2dc2126457760fde622df3a443d28
Noiredayz <noiredayz@protonmail.com>
2021-06-18 20:59:14 +0200

    IPPM: removed external channel specific code

--- internal-postprocess-message/code.js
+++ internal-postprocess-message/code.js
@@ -15,43 +15,7 @@ switch(target_context){
 		incmd	= cmdline.split(" ");
 		break;
 }
-const raid_regexp = RegExp('A Raid Event at Level \\[[0-9]+\\] has appeared*'); 
-
-/*
-if (target_channel === nlt.chctl.findChannel("supinic", "twitch") && unick === "supibot" && String(cmdline).trim()==="ppCircle"){
-	nlt.ss["twitch"].postmsg(target_channel, `ppSlide . o O ( ppCircle )`);
-	resolve("handled");
-	return;
-}
-*/
-if(target_channel === nlt.chctl.findChannel("supinic", "twitch") ){
-	if (unick === "wanductbot" && cmdline === "ppCircle"){
-		nlt.ss["twitch"].postmsg(target_channel, `ppCircle . o O ( ppSlide )`);
-		resolve("handled");
-		return;
-	}
-	if (unick === "stonks_bot500" && cmdline === "AlienPls Bot started AlienPls"){
-		nlt.ss["twitch"].postmsg(target_channel, `AlienPls`);
-		resolve("handled");
-		return;
-	}
-	if(unick==="huwobot" && cmdline.match(raid_regexp)){
-		nlt.ss["twitch"].postmsg(target_channel, `+join TO ARMS! SirShield FeelsDankMan SirSword`);
-		nlt.ss["twitch"].postmsg(target_channel, `elfidelfie alecbirdman griphthefrog FeelsDankMan 🔔 join raid`);
-		resolve("handled");
-		return;
-	}
-	if(unick==="huwobot" && cmdline.includes("failed to beat the raid level")){
-		nlt.ss["twitch"].postmsg(target_channel, `peepoSadDank`);
-		resolve("handled");
-		return;
-	}
-	if(unick==="huwobot" && cmdline.includes("users beat the raid level")){
-		nlt.ss["twitch"].postmsg(target_channel, `KomodoHype`);
-		resolve("handled");
-		return;
-	}
-}	
+const raid_regexp = RegExp('A Raid Event at Level \\[[0-9]+\\] has appeared*'); 	
 
 if (target_channel === nlt.chctl.findChannel("noires_bot", "twitch") && cmdline.match(RegExp('NoireWut'))){
 	nlt.ss["twitch"].postmsg(target_channel, `SmugNoire`);
@@ -64,13 +28,6 @@ if (target_channel === nlt.chctl.findChannel("fabzeef", "twitch") && unick === "
 	resolve("handled");
 	return;
 }
-
-if (target_channel === nlt.chctl.findChannel("senderak", "twitch") && unick === "senderakbot" && String(cmdline).trim()==="dankClappers"){
-	nlt.ss["twitch"].postmsg(target_channel, `PETTHEDANK`);
-	resolve("handled");
-	return;
-}
- 
 
 resolve("not handled");
 return;
