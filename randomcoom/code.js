eb95e347bec3fe22c1aefda7649bd4be89ea2e37
Noiredayz <noiredayz@protonmail.com>
2021-09-20 14:07:38 +0200

    rc: check thumbnail if it exists before checks

--- randomcoom/code.js
+++ randomcoom/code.js
@@ -1,6 +1,6 @@
 const {LOG_NO, LOG_DBG, LOG_INFO, LOG_WARN} = require(process.cwd()+"/lib/nlt-const.js");
 const {printtolog, donktime, getunixtime, getRndInteger} = require(process.cwd()+"/lib/nlt-tools.js");
-const {getRandomUserAgent, nsfwCheckURL} = require(process.cwd()+"/lib/nlt-got.js");
+const {getRandomUserAgent, nsfwCheckURL, thumbnailExists} = require(process.cwd()+"/lib/nlt-got.js");
 
 
 exports.noirelight2_command_code = function(fullmsg, unick, target_channel, target_context){
@@ -46,6 +46,18 @@ if(chlist.length===0){
 const ctarget = chlist[getRndInteger(0, chlist.length-1)];
 const tURL = `https://static-cdn.jtvnw.net/previews-ttv/live_user_${ctarget.user_login}-1920x1080.jpg`;
 let retval;
+try{
+	retval = await thumbnailExists(tURL);
+}
+catch(err){
+	printtolog(LOG_WARN, `<streamthumb> http error while trying to check if thumbnaile exists: ${err}`);
+	resolve(`rolled channel @${ctarget.user_login} but couldn't check if its thumbnail is available, so not doing the nsfw check. You can still get the thumb using ${nlt.c.cmd_prefix}st ${ctarget.user_login}`);
+	return;
+}
+if(!retval){
+	resolve(`rolled channel @${ctarget.user_login} is offline or doesn't have a thumbnail yet.`);
+	return;
+}
 try{
 	retval = await nsfwCheckURL(tURL);
 }
