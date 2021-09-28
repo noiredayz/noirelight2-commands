eba77408e9b86940df9829460b6779b5cf010ce5
Noiredayz <noiredayz@protonmail.com>
2021-09-29 01:29:03 +0200

    more verypog variations VeryPag

--- verypog/code.js
+++ verypog/code.js
@@ -1,7 +1,7 @@
 exports.noirelight2_command_code = function(fullmsg, unick, target_channel, target_context){
 return new Promise ((resolve, reject) => {
 
-	resolve("the list: AbyssPag FingerLickingPog Pogging ThanosPag VERYPOGGING VeryBased VeryBog VeryDonk VeryPag VeryPeg VeryPog VeryPogArrive VeryPogChamp VeryPoog NotVeryPog pogCheck tooPog");
+	resolve("the list: AbyssPag FingerLickingPog Pogging ThanosPag VERYPOGGING VeryBased VeryBog VeryDonk VeryPag VeryPeg VeryPog VeryPogArrive VeryPogChamp VeryPoog Vrpg VeryWide VeryFU VeryThinking VeryKek NotVeryPog pogCheck tooPog");
 
 })
 }
