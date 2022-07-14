2557f35df7a5bf73171c5cdc6bdbf6b9f2cbaca6
Noiredayz <noiredayz@protonmail.com>
2022-07-14 20:37:00 +0200

    whois: refactor + new function

--- whois/code.js
+++ whois/code.js
@@ -1,10 +1,10 @@
 const {LOG_NO, LOG_DBG, LOG_INFO, LOG_WARN} = require(process.cwd()+"/lib/nlt-const.js");
-const {printtolog, dcmdr} = require(process.cwd()+"/lib/nlt-tools.js");
+const {printtolog, dcmdr, validateTwitchUsername} = require(process.cwd()+"/lib/nlt-tools.js");
 const {helixGetData} = require(process.cwd()+"/lib/nlt-got.js");
 
 
 exports.noirelight2_command_code = function(fullmsg, unick, target_channel, target_context){
-return new Promise ((resolve, reject) => {
+return new Promise (async (resolve, reject) => {
 
 let cmdline, incmd;
 
@@ -24,32 +24,59 @@ if(incmd.length<2){
 		resolve(dcmdr("failed", false, "cmdfail", `Usage: ${nlt.c.cmd_prefix+incmd[0]} username`));
 		return;
 	}
-if(incmd[1].length<3 || incmd[1].length>25){
-		resolve(dcmdr("failed", false, "cmdfail", `parameter is too long or too short. Valid twitch usernames are between 3 and 25 characters.`));
+if(!validateTwitchUsername(incmd[1])){
+		resolve(dcmdr("failed", false, "cmdfail", `parameter is not a valid Twitch username.`));
 		return;
 }
 
-let retval="";
+let retval="", data;
 
-helixGetData("users", "login="+incmd[1]).then((data) => {
-	if(!data){
-		resolve(dcmdr("success", false, "normal", `User was not found or is currently banned.`));
+//TODO: refactor to proper async await
+try{
+	data = await helixGetData("users", "login="+incmd[1]);
+}
+catch(err){
+	printtolog(LOG_WARN, `<whois> Helix error: ${err}`);
+	resolve(dcmdr("failed", false, "cmdfail", `error quering Twitch API`));
+	return;
+}
+if(!data){	//Helix gave us back an empty set, could mean 2 things: 1. user is banned/deactivated 2. account doesn't exist fo sho
+	try{
+		retval = await nlt.got.get(`https://api.ivr.fi/v2/twitch/user?login=`+incmd[1]).json();
+	}
+	catch(err){
+		printtolog(LOG_WARN, `<whois> Error querying IVR: ${err}`);
+		resolve(dcmdr("success", false, "normal", `Couldn't find this user using Twitch's API and there was an error checking with IVR. If this user exists they are probably banned.`));
 		return;
 	}
-	const d = data[0];
-	retval = `@${d.login}`;
-	if(d.login != d.display_name.toLowerCase()) retval+=` (${d.display_name})`;
-	if(d.broadcaster_type) retval += `, role: ${d.broadcaster_type}`;
-	if(d.type) retval += `(${d.type}) `;
-	retval+= `, ID: ${d.id}, account created at: ${d.created_at.substr(0, 10)}, channel description: ${d.description?d.description:"(empty)"}`;
-	resolve(dcmdr("success", false, "normal", retval));
+	if(retval.length===0){
+		resolve(dcmdr("success", false, "normal", `User was not found eShrug`));
+		return;
+	} else {
+		let rs=`ID: ❌${retval[0].id} (${statusStr(retval[0].banReason)})`;
+		resolve(dcmdr("success", false, "normal", rs));
+		return;
+	}
 	return;
-}).catch((err) =>{
-	printtolog(LOG_WARN, `<helix> Error in whois command: ${err}`);
-	resolve(dcmdr("errored", false, "cmdfail", `API or HTTP error while trying to query Twitch`));
-	return;
-});
+}
+const d = data[0];
+retval = `@${d.login}`;
+if(d.login != d.display_name.toLowerCase()) retval+=` (${d.display_name})`;
+if(d.broadcaster_type) retval += `, role: ${d.broadcaster_type}`;
+if(d.type) retval += `(${d.type}) `;
+retval+= `, ID: ${d.id}, account created at: ${d.created_at.substr(0, 10)}, channel description: ${d.description?d.description:"(empty)"}`;
+resolve(dcmdr("success", false, "normal", retval));
+return;
 
 })
+}
+
+const statusTable = { TOS_INDEFINITE: "indefinitely banned",
+					  TOS_TEMPORARY: "temporarily banned",
+					  DEACTIVATED: "account deactivated by the user" };
+
+function statusStr(instr){
+	if(statusTable[instr]) return statusTable[instr];
+	else return instr;
 }
 
