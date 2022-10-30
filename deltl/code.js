d8d666d2b7db0aaa4a7f19302d370428c41b0a11
Noiredayz <noiredayz@protonmail.com>
2022-10-30 20:13:31 +0100

    classic

--- deltl/code.js
+++ deltl/code.js
@@ -24,7 +24,7 @@ if(cmdline.length<2){
 	resolve(`usage: ${nlt.c.cmd_prefix}deltl <url>`);
 	return;
 }
-const retval = nlt.tldb.insertQuery(`DELETE FROM tl WHERE url LIKE '%$`+cmdline[1]+`%';`);
+const retval = nlt.tldb.insertQuery(`DELETE FROM tl WHERE url LIKE '%`+cmdline[1]+`%';`);
 if(retval.changes>0){
 	resolve(`Successfully deleted ${retval.changes} entries.`);
 	return;
